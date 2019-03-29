a84b305dff35d6e248d7de263c241c9ff1b7e1d5
hofric15 <hofric15@htlkaindorf.at>
2019-03-29 12:17:59 +0100

    Noch einmal commiten wegen dem Ding und so

--- src/main/java/queue/MyQueue.java
+++ src/main/java/queue/MyQueue.java
@@ -5,10 +5,31 @@
  */
 package queue;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author Richard Hofmeister
  */
-public class MyQueue {
+public class MyQueue<T> {
+     private final LinkedList<T> data = new LinkedList<>();
+    private final int maxSize;
+
+    public MyQueue(int maxSize) {
+        this.maxSize = maxSize;
+    }
     
+    public void put(T value) throws FullException{
+        if( data.size() == maxSize ){
+            throw new FullException();
+        }
+        data.add(value);
+    }
+    
+    public T get() throws EmptyException{
+        if( data.isEmpty() ){
+            throw new EmptyException();
+        }
+        return data.poll();
+    }
 }
